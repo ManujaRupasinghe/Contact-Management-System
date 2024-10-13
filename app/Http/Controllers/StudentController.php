d8704ac89a76c5b661c0f885d64cf3a02152f4c0
tharindu-prasad <tharinduedirisinghe479@gmail.com>
2024-10-13 19:37:07 +0530

    modified:   app/Http/Controllers/StudentController.php modified:   app/Http/Requests/StoreStudentRequest.php 	new file:   resources/views/student/create.blade.php 	new file:   resources/views/student/index.blade.php 	new file:   resources/views/student/layout.blade.php 	modified:   routes/web.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,9 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreStudentRequest;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    
+    public function index() {
+        $students = Student::all();
+        return view('student.index', compact('students'));
+    }
+
+    public function create()  {
+        return view('student.create');
+    }
+
+    public function store(StoreStudentRequest $request) {
+        Student::create($request->validated());
+        return redirect('student')->with('success', 'Contact created successfully.');
+    }
+
+    public function all() {
+        return Student::all();
+    }
 }

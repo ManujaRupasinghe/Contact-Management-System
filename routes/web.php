b771b0215fff628e2c72332d67ca150ca7afb617
ManujaRupasinghe <manujarupasinghe@gmail.com>
2024-10-19 09:45:05 +0530

    create routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('contact', [StudentController::class, 'index']);
+Route::get('contact/create', [StudentController::class, 'create']);
+Route::get('contact/{id}/edit', [StudentController::class, 'edit']);
+Route::post('contact/store', [StudentController::class, 'store']);
+Route::get('contact/all', [StudentController::class, 'all']);
+Route::get('contact/{id}/get', [StudentController::class, 'get']);
+Route::post('contact/update/{id}', [StudentController::class, 'update']);
+Route::delete('contact/delete/{id}', [StudentController::class, 'delete']);
 

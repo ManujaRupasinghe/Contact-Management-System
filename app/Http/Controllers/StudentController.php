ab8899a4797c4897039366dd6f677a3135566b0d
Uc54834 <uchithasahan1235@gmil.com>
2024-10-20 20:59:01 +0530

    update function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,9 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+use App\Models\Student;
+use App\Http\Request\storeStudentRequest;
 
 class StudentController extends Controller
 {
-    
+    public function edit($id){
+        $student = Student::findOrFail($id);
+        return view('student.edit',student('student'));
+    }
+
+
+    public function update(Request $request, $id){
+        $student= Student::findOrFail($id);
+        $student->update($request->all());
+        return redirect('student')->with('success', 'Student contact details updated.');
+    }
 }

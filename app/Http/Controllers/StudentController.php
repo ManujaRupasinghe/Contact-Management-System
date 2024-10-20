77bcf26eb92274c38afa29fd0d2efab2964ad1fe
wijekoon824 <wijekoontharinduchamuditha@gmail.com>
2024-10-20 13:09:20 +0530

    first commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,5 +5,18 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    
+    public function index(){
+        $contact = Student::all();
+        return view('contact.index',compact('contact'));
+    }
+
+    public function all() 
+    { 
+        return Student::all(); 
+    } 
+ 
+    public function get($id) 
+    { 
+        return Student::findOrFail($id); 
+    } 
 }

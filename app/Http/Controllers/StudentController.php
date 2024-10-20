1ba7ad28163dd484e89abcdbf264208a107e5d7a
chamikara21 <chamikaramihiranga3@gmail.com>
2024-10-20 18:20:09 +0530

    Delete function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,49 +8,10 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index() {
-        $students = Student::all();
-        return view('student.index', compact('students'));
-    }
-
-    public function create() {
-        return view('student.create');
-    }
-
-    public function store(StoreStudentRequest $request) {
-        Student::create($request->validated());
-        return redirect()->route('student.index')->with('success', 'Contact created successfully.');
-    }
-
-    public function edit($id) {
-        $student = Student::findOrFail($id);
-        return view('student.edit', compact('student'));
-    }
-    
-    public function update(Request $request, $id) {
-        $validatedData = $request->validate([
-            'name' => 'required|string',
-            'phone' => 'required|string|regex:/^[0-9]+$/', // Allows only digits
-            'email' => 'nullable|string',
-            'address' => 'nullable|string',
-        ]);
-    
-        // Convert phone to integer if needed
-        $validatedData['phone'] = (int) $validatedData['phone']; 
-    
-        $student = Student::findOrFail($id);
-        $student->update($validatedData);
-    
-        return redirect('student')->with('success', 'Contact updated successfully.');
-    }
-    
-    public function destroy($id) {
-        $student = Student::findOrFail($id);
-        $student->delete();
-    
+    public function delete($id){
+        $contact = Student::findOrFail($id);
+        $contact->delete();
         return redirect('student')->with('success', 'Contact deleted successfully.');
     }
-    
-    
-    
+      
 }
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,4 @@
 
 use App\Http\Controllers\StudentController;
 
-Route::get('student', [StudentController::class, 'index'])->name('student.index');
-Route::get('student/create', [StudentController::class, 'create'])->name('student.create');
-Route::post('student/store', [StudentController::class, 'store'])->name('student.store');
-
-// update
-Route::get('student/{id}/edit', [StudentController::class, 'edit'])->name('student.edit');
-Route::put('student/{id}', [StudentController::class, 'update'])->name('student.update');
-
-// delete
-Route::delete('student/{id}', [StudentController::class, 'destroy'])->name('student.destroy');
+Route::delete('student/delete/{id}', [StudentController::class, 'delete']);

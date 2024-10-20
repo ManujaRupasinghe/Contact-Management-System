9528c1b96a36038c15c55a653f5826eefd056d58
tharindu-prasad <tharinduedirisinghe479@gmail.com>
2024-10-20 17:42:03 +0530

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,8 @@
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('student',[StudentController::class, 'index']);
-Route::get('student/create', [StudentController::class, 'create']);
 
-Route::post('student/store', [StudentController::class, 'store']);
-
-Route::get('student/all',[StudentController::class, 'all']);
-Route::get('student/{id}/get',[StudentController::class, 'get']);

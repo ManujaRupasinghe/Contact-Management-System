77f032be2fcbb947c23793af3afd4080cbeb3a97
ManujaRupasinghe <manujarupasinghe@gmail.com>
2024-10-26 19:18:43 +0530

    Changes

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,10 +1,41 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Student;
 use Illuminate\Http\Request;
 use App\http\Requests\StoreStudentRequest;
 
 class StudentController extends Controller
 {
-    
+    public function index(){
+        $student = Student::all();
+        return view('student.index',compact('student'));
+    }
+
+    public function all() 
+    { 
+        return Student::all(); 
+    } 
+ 
+    public function get($id) 
+    { 
+        return Student::findOrFail($id); 
+    } 
+
+    public function create()  {
+        return view('student.create');
+    }
+
+    public function store(StoreStudentRequest $request) {
+        Student::create($request->validated());
+        return redirect('student')->with('success', 'Contact created successfully.');
+    }
+
+    public function delete($id){
+        $student = Student::findOrFail($id);
+        $student->delete();
+        return redirect('student')->with('success', 'Contact deleted successfully.');
+    }
+      
 }
